359a0a552dc369103fd63e9377aac26c299ede48
DaveOkpare <okparedave@gmail.com>
2023-09-20 02:18:32 +0100

    Blackify script

--- process_doc.py
+++ process_doc.py
@@ -1,5 +1,5 @@
 import os
-from typing import List
+from typing import List, Any
 
 import openai
 from dotenv import load_dotenv
@@ -14,7 +14,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def create_schema_model(fields, field_name="Document"):
     labels = [i.strip() for i in fields.split(",")]
     model = openai_schema(
-        create_model(field_name, **{item: (type(item), ...) for item in labels})
+        create_model(field_name, **{item: (Any, ...) for item in labels})
     )
     multi_model = openai_schema(create_model(f"{field_name}s", data=(List[model], ...)))
     return multi_model
@@ -40,5 +40,7 @@ def process_extraction(fields, _obj, content):
 
 
 def process_qa(content):
-    result = process_extraction(fields="question, answer", _obj="Question", content=content)
+    result = process_extraction(
+        fields="question, answer", _obj="Question", content=content
+    )
     return result
