e037694f0f4063defc162d7a728bbbc3c6bef9f1
DaveOkpare <okparedave@gmail.com>
2023-09-20 02:21:24 +0100

    Remove record option

--- main.py
+++ main.py
@@ -8,16 +8,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 aai.settings.api_key = os.getenv("ASSEMBLYAI_API_KEY")
-
 transcriber = aai.Transcriber()
 
-tab1, tab2 = st.tabs(["Upload", "Record"])
+audio = st.file_uploader("Upload an audio file", type=["mp3"])
 
-with tab1:
-    audio = st.file_uploader("Upload an audio file", type=["mp3"])
-
-with tab2:
-    pass
 
 def get_transcript():
     with NamedTemporaryFile(suffix="mp3") as temp:
